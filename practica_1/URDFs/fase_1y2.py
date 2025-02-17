f8d2a8feb6a9831feb51c3f0ad045739a201b391
adrinava <a.navarredonda.2022@alumnos.urjc.es>
2025-02-17 12:49:27 +0100

    plots

--- practica_1/URDFs/fase_1y2.py
+++ practica_1/URDFs/fase_1y2.py
@@ -1,7 +1,6 @@
 import pybullet as p
 import pybullet_data
 import numpy as np
-import matplotlib as plt
 import time
 
 physicsClient = p.connect(p.GUI) #or p.DIRECT for non-graphical version
@@ -15,15 +14,19 @@ metaId = p.loadURDF("meta.urdf", useFixedBase=True)
 barreraId = p.loadURDF("barrera.urdf", useFixedBase=True)
 rampaID = p.loadURDF("rampa.urdf", useFixedBase=True)
 
-posicion_meta = [20, 0, 0]
-orientacion_meta = p.getQuaternionFromEuler([0,0,1.57])
+posicion_husky = [0, 0, 1]
+orientacion_husky = p.getQuaternionFromEuler([0, 0, 1.57])
 
-posicion_barrera = [17, 1.5, 0]
-orientacion_barrera = p.getQuaternionFromEuler([0, 0,-1.57])
+posicion_meta = [0, 20, 0]
+orientacion_meta = p.getQuaternionFromEuler([0,0,0])
 
-posicion_rampa = [10, 0, 0]
-orientacion_rampa = p.getQuaternionFromEuler([0,0,1.57])
+posicion_barrera = [1.5, 17, 0]
+orientacion_barrera = p.getQuaternionFromEuler([0, 0,3.14])
 
+posicion_rampa = [0, 10, 0]
+orientacion_rampa = p.getQuaternionFromEuler([0,0,0])
+
+p.resetBasePositionAndOrientation(huskyId, posicion_husky, orientacion_husky)
 p.resetBasePositionAndOrientation(metaId, posicion_meta, orientacion_meta)
 p.resetBasePositionAndOrientation(barreraId, posicion_barrera, orientacion_barrera)
 p.resetBasePositionAndOrientation(rampaID, posicion_rampa, orientacion_rampa)
@@ -32,6 +35,9 @@ p.setRealTimeSimulation(1)
 
 # 2 - front_left_wheel, 3 - front_right_wheel, 4 - rear_left_wheel, 5 - rear_right_wheel
 joints = [2, 3, 4, 5]
+
+data = []
+start_time = time.time()
 
 # Posicion inicial del husky
 prev_husky_pos, _ = p.getBasePositionAndOrientation(huskyId)
@@ -44,12 +50,20 @@ try:
         
         # Posicion actual del husky
         husky_pos, _ = p.getBasePositionAndOrientation(huskyId)
-        if husky_pos[0] - prev_husky_pos[0] >= 0.01:
+        if husky_pos[1] - prev_husky_pos[1] >= 0.01:
             # Tomar tiempo, posicion, velocidad, velocidad ruedas, fuerza ruedas
             pos_husky_y = husky_pos[1] # Posicion robot en eje Y
 
+            prev_husky_pos = husky_pos
 
-        prev_husky_pos = husky_pos
+            # Calculo de tiempo actual
+            actual_time = time.time() - start_time
+            start_time = time.time()
+
+            # Insertar datos en lista
+            output = p.getJointStates(huskyId, joints)
+            data.append([actual_time, pos_husky_y, p.getBaseVelocity(huskyId)[0][1]] + [10, 10, 10, 10] + [joint[1] for joint in output])
+
 
         # Terminar al tocar a la meta
         contacts = p.getContactPoints(bodyA=huskyId, bodyB=metaId)
@@ -58,5 +72,10 @@ try:
             
 except KeyboardInterrupt:
       pass
+
+np_data = np.array(data, dtype=float)
+
+# Guardar en un archivo CSV asegurando formato numérico
+np.savetxt("ex1.csv", np_data, delimiter=",", fmt="%.6f")
 	
 p.disconnect()
